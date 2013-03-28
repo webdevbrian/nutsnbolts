89ddf52c45d348557b6a157690b2ba43df63132a
Brian <webdevbrian@gmail.com>
2013-03-28 10:31:11 -0300

    Update header.php
    Included meta description, author and viewport as well as <IE9 HTML5 shim.

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,12 @@
 <html lang="en-us">
 <head>
 	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo $pageTitle; ?></title>
 	<link rel="stylesheet" href="css/reset.css" />
 	<?php // Feel free to remove normalize and add in your own such as bootstrap, etc. ?>
 	<link rel="stylesheet" href="css/normalize.css" />
-	<link rel="stylesheet" href="css/style.css" /> 
+	<link rel="stylesheet" href="css/style.css" />
+	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
